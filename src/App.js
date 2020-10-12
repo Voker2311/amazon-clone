b9a5c3708c7d1d1af8da37b58319e1702a7f8364
Akshay <akshaydeepakshinde@gmail.com>
2020-10-12 18:37:33 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Switch>
+            <Route path="/checkout">
+                <h1>Checkout portion</h1>
+            </Route>
+            <Route path="/login">
+                <h1>Login Page</h1>
+            </Route>
+            {/* This is a default Route */}
+           <Route path="/">
+                <Header />
+
+            </Route>
+        </Switch>
+      </div>
+    </Router>
+  );  
 }
 
 export default App;
